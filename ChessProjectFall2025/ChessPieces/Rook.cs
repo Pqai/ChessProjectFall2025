139d3e7b909449de885eb1bd264d50339fd920be
Jiego Pasamanero <83421019+Pqai@users.noreply.github.com>
2025-11-30 23:06:40 -0700

    Very Basic Structure can sstill be reworked

---
+++ ChessProjectFall2025/ChessPieces/Rook.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChessProjectFall2025.ChessPieces
+{
+    public class Rook : ChessPiece
+    {
+    }
+}

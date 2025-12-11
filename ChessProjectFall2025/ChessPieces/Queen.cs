ca8636d8ffd9cee5f1d32b9e62dbe5724a095796
Jiego Pasamanero <83421019+Pqai@users.noreply.github.com>
2025-12-11 16:46:28 -0700

    temp drawings for all pieces given

--- ChessProjectFall2025/ChessPieces/Queen.cs
+++ ChessProjectFall2025/ChessPieces/Queen.cs
@@ -1,6 +1,7 @@
 ﻿using ChessProjectFall2025.BaseGameLogic;
 using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -22,7 +23,50 @@ namespace ChessProjectFall2025.ChessPieces
 
         public override void Draw(PaintEventArgs e)
         {
-            throw new NotImplementedException();
+            Graphics g = e.Graphics;
+
+            //pawn Base
+            Point bottomLeft = new Point(Center.X - 20, Center.Y + 30);
+            Point bottomRight = new Point(Center.X + 20, Center.Y + 30);
+            Point baseTopL = new Point(Center.X - 20, Center.Y + 25);
+            Point baseTopR = new Point(Center.X + 20, Center.Y + 25);
+
+            //pawn body
+            Point bottomBodyL = new Point(Center.X - 15, Center.Y + 25);
+            Point bottomBodyR = new Point(Center.X + 15, Center.Y + 25);
+            Point topBodyL = new Point(Center.X - 12, Center.Y + 10);
+            Point topBodyR = new Point(Center.X + 12, Center.Y + 10);
+
+            //pawn head
+            Point headLeft = new Point(Center.X - 15, Center.Y - 5);
+            Point headRight = new Point(Center.X + 15, Center.Y - 5);
+            Point headTop = new Point(Center.X + 0, Center.Y - 10);
+
+            Point[] pawn = new Point[]
+            {
+                bottomLeft,
+                baseTopL,
+                bottomBodyL,
+                topBodyL,
+                headLeft,
+                headTop,
+                headRight,
+                topBodyR,
+                bottomBodyR,
+                baseTopR,
+                bottomRight,
+            };
+
+            using (var brush = new SolidBrush(PieceColorValue))
+            {
+                e.Graphics.FillPolygon(brush, pawn);
+            }
+
+            //outline
+            using (var pen = new Pen(OutlineColor, 2))
+            {
+                e.Graphics.DrawPolygon(pen, pawn);
+            }
         }
 
         public override List<BoardPosition> GetValidMoves(ChessBoard board)

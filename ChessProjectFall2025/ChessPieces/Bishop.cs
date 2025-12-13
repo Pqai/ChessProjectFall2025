38288b4435956aaf1a6707d9297171ea7284c395
Jiego Pasamanero <83421019+Pqai@users.noreply.github.com>
2025-12-12 18:16:41 -0700

    bishop drawing done

--- ChessProjectFall2025/ChessPieces/Bishop.cs
+++ ChessProjectFall2025/ChessPieces/Bishop.cs
@@ -47,35 +47,56 @@ namespace ChessProjectFall2025.ChessPieces
             Graphics g = e.Graphics;
 
             //Bishop Base
-            Point bottomLeft = new Point(ScreenPosition.X - 20, ScreenPosition.Y + 30);
-            Point bottomRight = new Point(ScreenPosition.X + 20, ScreenPosition.Y + 30);
-            Point baseTopL = new Point(ScreenPosition.X - 20, ScreenPosition.Y + 25);
-            Point baseTopR = new Point(ScreenPosition.X + 20, ScreenPosition.Y + 25);
+            Point BaseSlantLeft = new Point(ScreenPosition.X - 25, ScreenPosition.Y + 25);
+            Point BaseSlantRight = new Point(ScreenPosition.X + 25, ScreenPosition.Y + 25);
+            Point baseWallLeft = new Point(ScreenPosition.X - 25, ScreenPosition.Y + 35);
+            Point baseWallRight = new Point(ScreenPosition.X + 25, ScreenPosition.Y + 35);
 
             //Bishop body
-            Point bottomBodyL = new Point(ScreenPosition.X - 15, ScreenPosition.Y + 25);
-            Point bottomBodyR = new Point(ScreenPosition.X + 15, ScreenPosition.Y + 25);
-            Point topBodyL = new Point(ScreenPosition.X - 12, ScreenPosition.Y + 10);
-            Point topBodyR = new Point(ScreenPosition.X + 12, ScreenPosition.Y + 10);
+            Point bottomBodyL = new Point(ScreenPosition.X - 16, ScreenPosition.Y + 25);
+            Point bottomBodyR = new Point(ScreenPosition.X + 16, ScreenPosition.Y + 25);
+            Point topBodyL = new Point(ScreenPosition.X - 8, ScreenPosition.Y + 5);
+            Point topBodyR = new Point(ScreenPosition.X + 8, ScreenPosition.Y + 5);
+            //weird bump before crown
+            Point bumpLeft = new Point(ScreenPosition.X - 10, ScreenPosition.Y + 0);
+            Point bumpRight = new Point(ScreenPosition.X + 10, ScreenPosition.Y + 0);
+            Point bumpLeftBackToCenter = new Point(ScreenPosition.X - 8, ScreenPosition.Y - 5);
+            Point bumpRightBackToCenter = new Point(ScreenPosition.X + 8, ScreenPosition.Y - 5);
 
             //Bishop head
-            Point headLeft = new Point(ScreenPosition.X - 15, ScreenPosition.Y - 5);
-            Point headRight = new Point(ScreenPosition.X + 15, ScreenPosition.Y - 5);
-            Point headTop = new Point(ScreenPosition.X + 0, ScreenPosition.Y - 10);
+            Point headLeft = new Point(ScreenPosition.X - 14, ScreenPosition.Y - 10);
+            Point headLeftHip = new Point(ScreenPosition.X - 12, ScreenPosition.Y - 15);
+            Point headCutLeft = new Point(ScreenPosition.X - 8, ScreenPosition.Y - 16);
+            Point headCutBL = new Point(ScreenPosition.X - 3, ScreenPosition.Y - 11);
+            Point headCutBR = new Point(ScreenPosition.X - 1, ScreenPosition.Y - 13);
+            Point headCutRight = new Point(ScreenPosition.X - 6, ScreenPosition.Y - 20);
+            Point topHead = new Point(ScreenPosition.X - 0, ScreenPosition.Y - 28);
+            Point headRightHip = new Point(ScreenPosition.X + 12, ScreenPosition.Y - 15);
+            Point headRight = new Point(ScreenPosition.X + 14, ScreenPosition.Y - 10);
 
             Point[] bishop = new Point[]
             {
-                bottomLeft,
-                baseTopL,
+                baseWallLeft,
+                BaseSlantLeft,
                 bottomBodyL,
                 topBodyL,
+                bumpLeft,
+                bumpLeftBackToCenter,
                 headLeft,
-                headTop,
+                headLeftHip,
+                headCutLeft,
+                headCutBL,
+                headCutBR,
+                headCutRight,
+                topHead,
+                headRightHip,
                 headRight,
+                bumpRightBackToCenter,
+                bumpRight,
                 topBodyR,
                 bottomBodyR,
-                baseTopR,
-                bottomRight,
+                BaseSlantRight,
+                baseWallRight
             };
 
             using (var brush = new SolidBrush(PieceColorValue))
